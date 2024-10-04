1db08ad55c8d08b2318c921d4922ab3cd0f98e08
Yash <yash@Yashs-MacBook-Air.local>
2024-10-04 11:44:22 +0530

    db changes

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+// const db_uri = process.env.MONGO_DB_URI as string
+const db_uri = process.env.MONGO_URI as string
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/tourism');
+    await mongoose.connect(db_uri);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
