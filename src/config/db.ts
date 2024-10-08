5def77173ce071bcf33d1e816e1f2af60e1267ce
angularvandan <vandankumar98522@gmail.com>
2024-10-08 17:42:32 +0530

    uri

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-const db_uri = process.env.MONGO_DB_URI as string
-// const db_uri = process.env.MONGO_URI as string
+// const db_uri = process.env.MONGO_DB_URI as string
+const db_uri = process.env.MONGO_URI as string
 const connectDB = async () => {
   try {
     await mongoose.connect(db_uri);
